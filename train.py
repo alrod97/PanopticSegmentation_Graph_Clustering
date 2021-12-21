16e37dab1153760cb280d8b1c40a35cc1308515e
Alberto Gonzalo Rodriguez Salgado <alberto.rodriguez-salgado@tum.de>
2021-12-21 14:29:10 +0100

    # Now basic training with epochs is possible # optimizer defined # Ground truths defined # Loss function cross entropy defined
    # Next: Get graph batches working

--- train.py
+++ train.py
@@ -14,56 +14,52 @@ def train_one_epoch(epoch, model, train_loader, optimizer, loss_fn):
     all_labels = []
     running_loss = 0.0
     step = 0
-    for _, batch in enumerate(tqdm(train_loader)):
+    for _, batch in enumerate(tqdm.tqdm(train_loader)):
         # Use GPU
         batch.to(device)
         # Reset gradients
         optimizer.zero_grad()
         # Passing the node features and the connection info
-        pred = model(batch.x.float(),
-                                batch.edge_attr.float(),
-                                batch.edge_index,
-                                batch.batch)
+        Adj = torch.ones((batch.x.size()[0], batch.x.size()[0]))
+        row, col = np.where(Adj)
+        coo = np.array(list(zip(row, col)))
+        edges = torch.from_numpy(np.reshape(coo, (2, -1)))
+
+        pred = model(batch.x , edges, batch.edge_attr[:, None])
         # Calculating the loss and gradients
-        loss = loss_fn(torch.squeeze(pred), batch.y.float())
+        # get ground truth labels edges
+        Adj_gt = torch.reshape(batch.y, (batch.x.size()[0], batch.x.size()[0]))
+
+        labels_gt = torch.zeros(edges.size()[1])
+        # print('--')
+        for index, edge_cur in enumerate(batch.edge_index.T):
+            if Adj_gt[edge_cur[0], edge_cur[1]] == 1:
+                labels_gt[index] = 1
+            else:
+                labels_gt[index] = 0
+
+        loss = loss_fn(torch.squeeze(pred[:, 0]), labels_gt.float())
         loss.backward()
         optimizer.step()
         # Update tracking
         running_loss += loss.item()
         step += 1
-        all_preds.append(np.rint(torch.sigmoid(pred).cpu().detach().numpy()))
-        all_labels.append(batch.y.cpu().detach().numpy())
-    all_preds = np.concatenate(all_preds).ravel()
-    all_labels = np.concatenate(all_labels).ravel()
+        print(loss.item())
+        #all_preds.append(np.rint(torch.sigmoid(pred).cpu().detach().numpy()))
+        #all_labels.append(batch.y.cpu().detach().numpy())
+
+    #all_preds = np.concatenate(all_preds).ravel()
+    #all_labels = np.concatenate(all_labels).ravel()
     #calculate_metrics(all_preds, all_labels, epoch, "train")
     return running_loss/step
 
 train_dataset = PanopticClusteringDataset(root='graph_data/')
 train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 model = GNN(in_features=3, out_features=32, dropout=0, alpha=0.2)
+loss_fn  = torch.nn.BCELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
-print(train_loader)
+epochs_total = np.arange(10)
+for epoch in epochs_total:
+    train_one_epoch(epoch, model, train_loader, optimizer, loss_fn)
 
-for i in train_loader:
-    # get xyz points
-    points = i.x
-    edge_index = i.edge_index
-    label = i.y
-    edge_feature = i.edge_attr
-    edge_feature = edge_feature[:, None]
-
-    print(edge_feature.size())
-
-    Adj = torch.reshape(label, (points.size()[0], points.size()[0]))
-    Adj = torch.ones((points.size()[0], points.size()[0]))
-    row, col = np.where(Adj)
-    coo = np.array(list(zip(row, col)))
-    edges = torch.from_numpy(np.reshape(coo, (2, -1)))
-    print(edges.size())
-    output = model(points, edges, edge_feature)
-
-    print(points.size())
-    print(output.size())
-    print('--')
-
-model = GNN(in_features=3, out_features=32, dropout=0, alpha=0.2)
